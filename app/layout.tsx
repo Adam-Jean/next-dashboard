0b16dd70847261cc3fda3b647922cb06a111f895
AJzBeast <adam123jean@gmail.com>
2025-06-18 16:35:29 -0400

    Fixed root layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import "@/app/ui/global.css";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next.js Dashboard",
+  description: "A modern dashboard built with Next.js",
+};
 
 export default function RootLayout({
   children,
